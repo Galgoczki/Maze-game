0ffc376c0a66d467ca30e46d090aec93d610b356
Galgoczki <norbigalgoczki@gmail.com>
2020-12-12 18:26:15 +0100

    dokumentáció,maze generálás haladás,1 modell hozzá adása
    csináltam egy kisebb dokumentációt(csunya) magamnak hogy gyorsabban átlássab ha ki kell hagynom egy kissebb idöt a fejlesztések között,a maze generálásával hadatam de még nem tökéletes és a különbözö tárgyakat még nem halyez el a pályán,hozzá adtam a játékos modellét,a mozgás animációt még nem kötöttem be

--- Szakdolgozat_Galgoczki_Norbert/Assets/scripts/mazegenerater.cs
+++ Szakdolgozat_Galgoczki_Norbert/Assets/scripts/mazegenerater.cs
@@ -4,29 +4,31 @@ using UnityEngine;
 
 public class Mazegenerater : MonoBehaviour
 {
-    public Transform playersTransforms;
+    [SerializeField]private Transform playersTransforms;
     public string key;//where we start the generat;how deep is the FILO go;where is the player start ;wher the filo is step;
-    public Vector3 playerStarterpont;
-    public bool keyalreadygiven = false;
-    public System.Random rnd = new System.Random();
-    public GameObject floor;
-    public GameObject roof;
-    public GameObject wall;
-    public GameObject floors;
-    public GameObject roofs;
-    public GameObject walls;
-    public bool[,,] basemaze;
+    [SerializeField]private Vector3 playerStarterpont;
+    [SerializeField]private bool keyalreadygiven = false;
+    private System.Random rnd = new System.Random();
+    [SerializeField]private GameObject floor;
+    [SerializeField]private GameObject roof;
+    [SerializeField]private GameObject wall;
+    [SerializeField]private GameObject floors;
+    [SerializeField]private GameObject roofs;
+    [SerializeField]private GameObject walls;
+    [SerializeField]private bool[,,] basemaze;
     //    _1_default
     // 0 |   | 2       its the cells wall numbers
     //   |_3_|
     // default pos of wall --
-    public int rows;
-    public int columns;
-    public float size = 6;//size of the maze's cells
+    [SerializeField]private int rows;
+    [SerializeField]private int columns;
+    [SerializeField]private float size = 6;//size of the maze's cells
+    public short[,] generateLeft;
     private float fullcellsize = 7;//cell+wallssize
     private Vector3 starterpoint = new Vector3(20,0,20);//the mazes starter corner
     private bool generateDone = false;
-    private short[,] generateLeft;
+    private int[] algorimusFilo;
+    private int positionInTheFiloArray = 0;
     // Start is called before the first frame update
     void Start(){
         rows    = rnd.Next(3,16);
@@ -54,7 +56,7 @@ public class Mazegenerater : MonoBehaviour
                 Instantiate(floor,new Vector3(starterpoint.x+(i*fullcellsize)+fullcellsize/2,starterpoint.y-0.5f,starterpoint.z+(j*fullcellsize)-fullcellsize/2),Quaternion.identity,floors.transform);
                 
                 //roof
-                Instantiate(roof,new Vector3(starterpoint.x+(i*fullcellsize)+fullcellsize/2,starterpoint.y+5.5f,starterpoint.z+(j*fullcellsize)-fullcellsize/2),Quaternion.identity,roofs.transform);
+                //Instantiate(roof,new Vector3(starterpoint.x+(i*fullcellsize)+fullcellsize/2,starterpoint.y+5.5f,starterpoint.z+(j*fullcellsize)-fullcellsize/2),Quaternion.identity,roofs.transform);
                 //walls 
                 if(basemaze[i,j,0]){// left
                     Instantiate(wall,new Vector3(starterpoint.x+(i*fullcellsize)+fullcellsize/2-3.25f,starterpoint.y+2.5f,starterpoint.z+(j*fullcellsize)-fullcellsize/2),Quaternion.Euler(0f,270f,0f),walls.transform); 
@@ -90,10 +92,11 @@ public class Mazegenerater : MonoBehaviour
         temporary = rnd.Next(1,14);
         key += temporary.ToString() + ",0;";
         //generat how deep is the FILO is go
+
         //here i have 2 idea with is flexiable to themaze size
         //temporary = Mathf.RoundToInt(Mathf.Pow((rows*columns),1f/2f));
         temporary = (Mathf.RoundToInt( Mathf.Pow( (rows*columns), 1f / 2f))+Mathf.RoundToInt( Mathf.Pow((rows+columns) , 1f / 2f)))/2;
-        //temporary = Mathf.RoundToInt(Mathf.Pow((rows+columns),1f/2f));
+         //temporary = Mathf.RoundToInt(Mathf.Pow((rows+columns),1f/2f));
         key += temporary.ToString()+";";
         //its will need in the last argument to the algoritm
         int range = (temporary*2)-1;
@@ -124,61 +127,248 @@ public class Mazegenerater : MonoBehaviour
                     playerStarterpontx= columns-1;
                 }
             }
-            //im not sure itt good
-            key +=playerStarterponty.ToString() +","+playerStarterpontx.ToString()+";";
+
+            key +=playerStarterponty.ToString() +","+playerStarterpontx.ToString()+";"; //where the player is start
 
         //where the FILO go
-        for(int i=0; i< range; i++){
-            temporary = rnd.Next(4);//all the sides left top right down
+        for(int i=0; i< range && i<11; i++){
+            temporary = rnd.Next(4);//all the sides left top right down (0,1,2,3)->(left top right bot)
             key += temporary.ToString();
             key += ",";
         }
-        key += "0;";
+        key += "0,1,2,3";//+";"; ha bovitjuk
     }
     void generate(){
         //split the key component to generat easily
-        string[] temporarystringholder = key.Split(';');
+        string[] temporarystringholder = key.Split(';');// the stats of the allgoritm param
         string[] temporarystringholder2;
+
         int[] algoritmStarterPoints;//fill up in foreach
-        temporarystringholder2=temporarystringholder[0].Split(',');
+
+        temporarystringholder2=temporarystringholder[0].Split(',');//first part of the key
         algoritmStarterPoints = new int[temporarystringholder2.Length];
         for (int i = 0; i < temporarystringholder2.Length; i++){
-            algoritmStarterPoints[i]=int.Parse(temporarystringholder2[i]);
+            algoritmStarterPoints[i]=int.Parse(temporarystringholder2[i]);//where we go
         }
-        int algorimusFiloDeepestPoint = int.Parse(temporarystringholder[1]);
-        temporarystringholder2=temporarystringholder[2].Split(',');
+        int deep = int.Parse(temporarystringholder[1]);//secund part of the key(the fifos deepes point)
+        temporarystringholder2=temporarystringholder[2].Split(',');//3th part of the key
         List<int[]> ListOfThePossiblaStarterPoints = new List<int[]>(); //its will be the list of the point where the algoritm possible start to grow the maze
         ListOfThePossiblaStarterPoints.Add(new int[2]{int.Parse(temporarystringholder2[0]),int.Parse(temporarystringholder2[1])});
         playerStarterpont = new Vector3(starterpoint.x+(ListOfThePossiblaStarterPoints[0][0]*fullcellsize)+fullcellsize/2,starterpoint.y+1.5f,starterpoint.z+(ListOfThePossiblaStarterPoints[0][1]*fullcellsize)-fullcellsize/2);
-        int[] algorimusFilo;//fill up in foreach
-        temporarystringholder2=temporarystringholder[3].Split(',');
+        //the first element of this list is the point where we start the maze generat and its the starter point of the player
+        
+        temporarystringholder2=temporarystringholder[3].Split(',');//4th part of the key and yet the last
         algorimusFilo =  new int[temporarystringholder2.Length];
         for (int i = 0; i < temporarystringholder2.Length; i++){
             algorimusFilo[i]=int.Parse(temporarystringholder2[i]);
         }
         //key read is done,we can start the algoritm
 
-        Debug.Log(ListOfThePossiblaStarterPoints[0][0]+" and " + ListOfThePossiblaStarterPoints[0][1]);
         playersTransforms.position = playerStarterpont;
         bool done=false;
-        while (!generateDone){//if a cell is done -> true
-            
-            
+        /*foreach (var a in algorimusFilo)
+        {
+            Debug.Log(a);
+        }*/
+        int prex=ListOfThePossiblaStarterPoints[0][0];
+        int prey=ListOfThePossiblaStarterPoints[0][1];
+        recursiveGenerate(ListOfThePossiblaStarterPoints[0][0],ListOfThePossiblaStarterPoints[0][1],ListOfThePossiblaStarterPoints[0][0],ListOfThePossiblaStarterPoints[0][1],deep);
+        ListOfThePossiblaStarterPoints.Clear();
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){//ne felejtsd el updatelni!!
+            bool needPutInTheTable=false;
+                if(generateLeft[i,j]==-1){//-1 is meaning the algoritm is now on this cells(the FILO part)
+                    if(i==0){
+                            if(generateLeft[i+1,j]==0){
+                                needPutInTheTable=true;
+                            }
+                    }else{
+                        if(i==rows-1){
+                            if(generateLeft[i-1,j]==0){
+                                needPutInTheTable=true;
+                            } 
+                        }else{
+                            if(generateLeft[i-1,j]==0||generateLeft[i+1,j]==0){
+                                needPutInTheTable=true;
+                            } 
+                        }
+                    }
+                            
+                    if(j==0){
+                            if(generateLeft[i,j+1]==0){
+                                needPutInTheTable=true;
+                            }
+                    }else{
+                        if(j==(columns-1)){
+                            if(generateLeft[i,j-1]==0){
+                                needPutInTheTable=true;
+                            }
+                        }else{
+                        if(generateLeft[i,j-1]==0||generateLeft[i,j+1]==0){
+                            needPutInTheTable=true;
+                        }
 
-            done = true;
-            //its done?
-            for (int i = 0; i < rows; i++){
-                for (int j = 0; j < columns; j++){//ne felejtsd el updatelni!!
-                    if(generateLeft[i,j]==0 ||generateLeft[i,j]==-1){
-                        done = false;
+                    }
+                }
+                if(needPutInTheTable){
+                    ListOfThePossiblaStarterPoints.Add(new int[2]{i,j});
+                }else{
+                    generateLeft[i,j]=-2;
+                }
+                }
+            }
+        }
+    /*foreach (var item in ListOfThePossiblaStarterPoints)
+    {
+        Debug.Log(item[0] + "és" +item[1]);
+    }*/
+    //--------------
+        int asd=1;
+        int szamlalo=0;
+        while (/*!generateDone &&*/ ListOfThePossiblaStarterPoints.Count>0 && szamlalo<20){//if a cell is done -> true
+            szamlalo++;
+            if(algoritmStarterPoints[asd]<ListOfThePossiblaStarterPoints.Count){//valamiért sosme lép bele
+                recursiveGenerate(ListOfThePossiblaStarterPoints[algoritmStarterPoints[asd]][0],ListOfThePossiblaStarterPoints[algoritmStarterPoints[asd]][1],ListOfThePossiblaStarterPoints[algoritmStarterPoints[asd]][0],ListOfThePossiblaStarterPoints[algoritmStarterPoints[asd]][1],deep);
+                
+            }else{
+                asd=(asd+1>2)?0:asd++;
+                if(algoritmStarterPoints[asd]<ListOfThePossiblaStarterPoints.Count){//valamiért sosme lép bele
+                    recursiveGenerate(ListOfThePossiblaStarterPoints[algoritmStarterPoints[asd]][0],ListOfThePossiblaStarterPoints[algoritmStarterPoints[asd]][1],ListOfThePossiblaStarterPoints[algoritmStarterPoints[asd]][0],ListOfThePossiblaStarterPoints[algoritmStarterPoints[asd]][1],deep);
+                    
+                }else{
+                    asd=(asd+1>2)?0:asd++;
+                    if(algoritmStarterPoints[asd]<ListOfThePossiblaStarterPoints.Count){//valamiért sosme lép bele
+                        recursiveGenerate(ListOfThePossiblaStarterPoints[algoritmStarterPoints[asd]][0],ListOfThePossiblaStarterPoints[algoritmStarterPoints[asd]][1],ListOfThePossiblaStarterPoints[algoritmStarterPoints[asd]][0],ListOfThePossiblaStarterPoints[algoritmStarterPoints[asd]][1],deep);
+                        
+                    }else{
+                       recursiveGenerate(ListOfThePossiblaStarterPoints[ListOfThePossiblaStarterPoints.Count-1][0],ListOfThePossiblaStarterPoints[ListOfThePossiblaStarterPoints.Count-1][1],ListOfThePossiblaStarterPoints[ListOfThePossiblaStarterPoints.Count-1][0],ListOfThePossiblaStarterPoints[ListOfThePossiblaStarterPoints.Count-1][1],deep);
+                     
                     }
                 }
             }
-            generateDone = true;//its just temerary
+            
+            
+
+            ListOfThePossiblaStarterPoints.Clear();
+            Debug.Log(ListOfThePossiblaStarterPoints.Count);
+            done = true;
+            //its done? and where we start next
+            for (int i = 0; i < rows; i++){
+                for (int j = 0; j < columns; j++){//ne felejtsd el updatelni!!
+                bool needPutInTheTable=false;
+                    if(generateLeft[i,j]==-1){//-1 is meaning the algoritm is now on this cells(the FILO part)
+                        if(i==0){
+                                if(generateLeft[i+1,j]==0){
+                                    done=false;
+                                    needPutInTheTable=true;
+                                }
+                        }else{
+                            if(i==rows-1){
+                               if(generateLeft[i-1,j]==0){
+                                    done=false;
+                                    needPutInTheTable=true;
+                                } 
+                            }else{
+                               if(generateLeft[i-1,j]==0||generateLeft[i+1,j]==0){
+                                    done=false;
+                                    needPutInTheTable=true;
+                                } 
+                            }
+                        }
+                                
+                        if(j==0){
+                                if(generateLeft[i,j+1]==0){
+                                    done=false;
+                                    needPutInTheTable=true;
+                                }
+                        }else{
+                            if(j==(columns-1)){
+                                if(generateLeft[i,j-1]==0){
+                                    done=false;
+                                    needPutInTheTable=true;
+                                }
+                            }else{
+                            if(generateLeft[i,j-1]==0||generateLeft[i,j+1]==0){
+                                done=false;
+                                needPutInTheTable=true;
+                            }
+
+                        }
+                    }
+                    if(needPutInTheTable){
+                        ListOfThePossiblaStarterPoints.Add(new int[2]{i,j});
+                    }else{
+                        generateLeft[i,j]=-2;
+                    }
+                    }
+                }
+            }
+            generateDone=done;
+            Debug.Log(ListOfThePossiblaStarterPoints.Count);
         }
+    //---------
+    string itemitem="";
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < columns; j++){
+            itemitem += ((generateLeft[i,j]==-1)?"c":(generateLeft[i,j]==-2)?"v":"#") + "  ";
+        }
+        itemitem+="\n";
     }
-    bool recursiveGenerate(int x,int y,int currentRecursivePoinLeft){//
-        if(currentRecursivePoinLeft==0){
+    Debug.Log(itemitem);
+    
+    //-----------
+    }
+    bool recursiveGenerate(int prex,int prey,int x,int y,int currentRecursivePoinLeft){//i didnt konw now why we give back bool
+        if(currentRecursivePoinLeft==0/*|| (generateLeft[x-1,y]!=0 && generateLeft[x,y-1] !=0 && generateLeft[x,y+1]!= 0 && generateLeft[x+1,y]!=0)*/){//if we are cant move forward or we didnt have left stap point
+            generateLeft[x,y]=-1;
+            return true;
+        }
+        bool movetonext = false;
+        for (int i = 0; i < 15 && !movetonext; i++){
+            int next=nextstep();
+            if(algorimusFilo[next]==0){//left
+                if((prex!=x-1)&&(x-1>=0)&&(generateLeft[x-1,y]==0)){//its possible
+                        basemaze[x,y,0]=false;//current cells left side
+                        basemaze[x-1,y,2]=false;//next cells right side
+                        generateLeft[x,y]=-1;//-1 => its mean we are now in this filo on there road;
+                        movetonext=true;
+                        recursiveGenerate(x,y,x-1,y,currentRecursivePoinLeft-1);
+                        //Debug.Log("az "+x+" és "+y+" helyröl kitoroljűk a 0 falat:"+basemaze[x,y,0]);
+                    }
+                }
+            if(algorimusFilo[next]==1){//up
+                if((prey!=y-1)&&(y-1>=0)&&(generateLeft[x,y-1]==0)){//its possible,if its possitiv we cant go there//i dont know why i writed this comment
+                    basemaze[x,y,1] = false;//current cells left side
+                    basemaze[x,y-1,3] = false;//next cells right side
+                    generateLeft[x,y]= -1;//-1 => its mean we are now in this filo on there road;
+                    movetonext=true;
+                    //Debug.Log("az "+x+" és "+y+" helyröl kitoroljűk a 1 falat:"+basemaze[x,y,1]);
+                    recursiveGenerate(x,y,x,y-1,currentRecursivePoinLeft-1);
+                }
+            }
+            if(algorimusFilo[next]==2){//right
+                if((prex!=x+1)&&(x+1<rows)&&(generateLeft[x+1,y]==0)){//its possible
+                    basemaze[x,y,2]=false;//current cells left side
+                    basemaze[x+1,y,0]=false;//next cells right side
+                    generateLeft[x,y]= -1;//-1 => its mean we are now in this filo on there road;
+                    movetonext=true;
+                    //Debug.Log("az "+x+" és "+y+" helyröl kitoroljűk a 2 falat:"+basemaze[x,y,2]);
+                    recursiveGenerate(x,y,x+1,y,currentRecursivePoinLeft-1);
+                }
+            }
+            if(algorimusFilo[next]==3){//down
+                if((prey!=y+1)&&(y+1<columns)&&(generateLeft[x,y+1]==0)){//its possible
+                    basemaze[x,y,3]=false;//current cells left side
+                    basemaze[x,y+1,1]=false;//next cells right side
+                    generateLeft[x,y]= -1;//-1 => its mean we are now in this filo on there road;
+                    movetonext=true;
+                    //Debug.Log("az "+x+" és "+y+" helyröl kitoroljűk a 3 falat:"+basemaze[x,y,3]);
+                    recursiveGenerate(x,y,x,y+1,currentRecursivePoinLeft-1);
+                }
+            }
+        }
+        if(!movetonext){
+            generateLeft[x,y]= -1;
             return true;
         }
         return false;
@@ -190,5 +380,13 @@ public class Mazegenerater : MonoBehaviour
 
     void generateRoom(){
 
+    }
+    int nextstep(){
+        positionInTheFiloArray += 1; //readable
+        if(positionInTheFiloArray<algorimusFilo.Length){
+            return positionInTheFiloArray;
+        }
+        positionInTheFiloArray=0;
+        return positionInTheFiloArray;
     }
 }
