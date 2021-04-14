ed31da833887a9cbbd7d2cfec8a06f45c5f6e296
Galgoczki <norbigalgoczki@gmail.com>
2021-04-14 18:12:04 +0200

    fixing normaly

--- Szakdolgozat_Galgoczki_Norbert/Assets/scripts/Global_options_handler.cs
+++ Szakdolgozat_Galgoczki_Norbert/Assets/scripts/Global_options_handler.cs
@@ -3,22 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 static class Global_options_handler{
-    private float sound;
-    float sensitivity;
-
-    void setSound(float sound){
-        this.sound=sound;
-    }
-
-    void setSensitivity(float sensitivity){
-        this.sensitivity=sensitivity;
-    }
-    float getSound(){
-        return this.sound;
-    }
-
-    float getSensitivity(){
-        return this.sensitivity;
-    }
+    static float sound;
+    static float sensitivity;
 
 }
