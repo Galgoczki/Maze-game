5aea9167f39db6d8c456efb8966eeefe72101301
Galgoczki <norbigalgoczki@gmail.com>
2021-04-14 18:08:19 +0200

    hang és érzékenység beálitása előkészűlödés
    csak a cime a osztály létrehozása

---
+++ Szakdolgozat_Galgoczki_Norbert/Assets/scripts/Global_options_handler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class Global_options_handler{
+    private float sound;
+    private float sensitivity;
+
+    public void setSound(float sound){
+        this.sound=sound;
+    }
+
+    public void setSensitivity(float sensitivity){
+        this.sensitivity=sensitivity;
+    }
+    public float getSound(){
+        return this.sound;
+    }
+
+    public float getSensitivity(){
+        return this.sensitivity;
+    }
+
+}
