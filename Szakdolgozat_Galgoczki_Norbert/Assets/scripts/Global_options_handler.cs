c48b375e77449135aace484503d41f40c5911735
Galgoczki <norbigalgoczki@gmail.com>
2021-04-14 18:09:35 +0200

    fixing a stupid thing

--- Szakdolgozat_Galgoczki_Norbert/Assets/scripts/Global_options_handler.cs
+++ Szakdolgozat_Galgoczki_Norbert/Assets/scripts/Global_options_handler.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 static class Global_options_handler{
     private float sound;
-    private float sensitivity;
+    float sensitivity;
 
-    public void setSound(float sound){
+    void setSound(float sound){
         this.sound=sound;
     }
 
-    public void setSensitivity(float sensitivity){
+    void setSensitivity(float sensitivity){
         this.sensitivity=sensitivity;
     }
-    public float getSound(){
+    float getSound(){
         return this.sound;
     }
 
-    public float getSensitivity(){
+    float getSensitivity(){
         return this.sensitivity;
     }
 
